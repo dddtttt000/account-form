67601d1627c452468143eb233edb6304999d890b
dddtttt000 <>
2021-08-20 11:49:55 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Create Account</h1>
+        <form onsubmit noValidate>
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              className="form-control"
+              placeholder="First Name"
+              type="text"
+              name="firstName"
+              noValidate
+              onchange
+            />
+          </div>
+
+          <div className="lastName">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last Name"
+              name="lastName"
+              noValidate
+              onchange
+            />
+          </div>
+
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              noValidate
+              onchange
+            />
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Password"
+              type="password"
+              name="password"
+              noValidate
+              onchange
+            />
+          </div>
+          <div className="createAccount">
+            <button type="submit" className>
+              Create Account
+            </button>
+            <small class>Already Have an Account?</small>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
